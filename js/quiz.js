76458ee7cdc87984d0ce171e41ef909868a6620c
Oratiwe <104420878+Oratiwe@users.noreply.github.com>
2024-04-13 23:25:22 +0200

    js updated

--- js/quiz.js
+++ js/quiz.js
@@ -1,144 +1,168 @@
 var quiz = {
-    // (A) PROPERTIES
-    // (A1) QUESTIONS & ANSWERS
-    // Q = QUESTION, O = OPTIONS, A = CORRECT ANSWER
-    data: [
+  // (A) PROPERTIES
+  // (A1) QUESTIONS & ANSWERS
+  // Q = QUESTION, O = OPTIONS, A = CORRECT ANSWER
+  data: [
     {
-      q : "What is the standard distance between the target and archer in Olympics?",
-      o : [
-        "50 meters",
-        "70 meters",
-        "100 meters",
-        "120 meters"
+      q: "OOP stands for :",
+      o: [
+        "Object observed Paradim",
+        "Open ordained parallel",
+        "Observation Oriented procedure",
+        "Object Oriented programming",
       ],
-      a : 1 // arrays start with 0, so answer is 70 meters
+      a: 3,
     },
     {
-      q : "Which is the highest number on a standard roulette wheel?",
-      o : [
-        "22",
-        "24",
-        "32",
-        "36"
+      q: "What term relates to polymorphism ?",
+      o: [
+        "Dynamic binding",
+        "Static typing",
+        "Dynamic allocation",
+        "Static alloction",
       ],
-      a : 3
+      a: 0,
     },
     {
-      q : "How much wood could a woodchuck chuck if a woodchuck would chuck wood?",
-      o : [
-        "150 KG",
-        "200 KG",
-        "350 KG",
-        "420 KG"
-      ],
-      a : 2
+      q: "What is the process by which one can acquire the properties of another object ?",
+      o: ["Encapsulation", "Inheritance", "Polymorphism"],
+      a: 1,
     },
     {
-      q : "Which is the seventh planet from the sun?",
-      o : [
-        "Uranus",
-        "Earth",
-        "Pluto",
-        "Mars"
-      ],
-      a : 0
+      q: "Information hiding can also be termed as what ?",
+      o: ["Inheritance", "Encapsulation", "Data hiding", "Polymorphism"],
+      a: 2,
     },
     {
-      q : "Which is the largest ocean on Earth?",
-      o : [
-        "Atlantic Ocean",
-        "Indian Ocean",
-        "Arctic Ocean",
-        "Pacific Ocean"
+      q: "What are constructors used for ?",
+      o: [
+        "Free memory",
+        "To build a user interface",
+        "Initialize a newly created object",
+        "To create a sub-class",
       ],
-      a : 3
+      a: 2,
+    },
+    {
+      q: "Which is the Keyword that is used to access the method variables from the sub-class ?",
+      o: ["is_a", "Has_a", "Super", "Using"],
+      a: 2,
+    },
+    {
+      q: "An object that has more than one form is referred to as what ?",
+      o: ["Abstract class", "Inheritance", "Polymorphism", "Interface"],
+      a: 2,
+    },
+    {
+      q: "The main method can be overhidden",
+      o: ["True", "False", "Maybe", "All of the above"],
+      a: 1,
+    },
+
+    {
+      q: "Choose an Object Oriented Programming language from the following:",
+      o: ["HTML", "Visual Basic", "English", "Java"],
+      a: 3,
+    },
+    {
+      q: "What is an Object ?",
+      o: [
+        "An instane of a class",
+        "A blueprint",
+        "Anything you want",
+        "All of the above",
+      ],
+      a: 0,
+    },
+  ],
+
+  // (A2) HTML ELEMENTS
+  hWrap: null, // HTML quiz container
+  hQn: null, // HTML question wrapper
+  hAns: null, // HTML answers wrapper
+
+  // (A3) GAME FLAGS
+  now: 0, // current question
+  score: 0, // current score
+
+  // (B) INIT QUIZ HTML
+  init: () => {
+    // (B1) WRAPPER
+    quiz.hWrap = document.getElementById("quizWrap");
+
+    // (B2) QUESTIONS SECTION
+    quiz.hQn = document.createElement("div");
+    quiz.hQn.id = "quizQn";
+    quiz.hWrap.appendChild(quiz.hQn);
+
+    // (B3) ANSWERS SECTION
+    quiz.hAns = document.createElement("div");
+    quiz.hAns.id = "quizAns";
+    quiz.hWrap.appendChild(quiz.hAns);
+
+    // (B4) GO!
+    quiz.draw();
+  },
+
+  // (C) DRAW QUESTION
+  draw: () => {
+    // (C1) QUESTION
+    quiz.hQn.innerHTML = quiz.data[quiz.now].q;
+
+    // (C2) OPTIONS
+    quiz.hAns.innerHTML = "";
+    for (let i in quiz.data[quiz.now].o) {
+      let radio = document.createElement("input");
+      radio.type = "radio";
+      radio.name = "quiz";
+      radio.id = "quizo" + i;
+      quiz.hAns.appendChild(radio);
+      let label = document.createElement("label");
+      label.innerHTML = quiz.data[quiz.now].o[i];
+      label.setAttribute("for", "quizo" + i);
+      label.dataset.idx = i;
+      label.addEventListener("click", () => {
+        quiz.select(label);
+      });
+      quiz.hAns.appendChild(label);
     }
-    ],
-  
-    // (A2) HTML ELEMENTS
-    hWrap: null, // HTML quiz container
-    hQn: null, // HTML question wrapper
-    hAns: null, // HTML answers wrapper
-  
-    // (A3) GAME FLAGS
-    now: 0, // current question
-    score: 0, // current score
-  
-    // (B) INIT QUIZ HTML
-    init: () => {
-      // (B1) WRAPPER
-      quiz.hWrap = document.getElementById("quizWrap");
-  
-      // (B2) QUESTIONS SECTION
-      quiz.hQn = document.createElement("div");
-      quiz.hQn.id = "quizQn";
-      quiz.hWrap.appendChild(quiz.hQn);
-  
-      // (B3) ANSWERS SECTION
-      quiz.hAns = document.createElement("div");
-      quiz.hAns.id = "quizAns";
-      quiz.hWrap.appendChild(quiz.hAns);
-  
-      // (B4) GO!
-      quiz.draw();
-    },
-  
-    // (C) DRAW QUESTION
-    draw: () => {
-      // (C1) QUESTION
-      quiz.hQn.innerHTML = quiz.data[quiz.now].q;
-  
-      // (C2) OPTIONS
-      quiz.hAns.innerHTML = "";
-      for (let i in quiz.data[quiz.now].o) {
-        let radio = document.createElement("input");
-        radio.type = "radio";
-        radio.name = "quiz";
-        radio.id = "quizo" + i;
-        quiz.hAns.appendChild(radio);
-        let label = document.createElement("label");
-        label.innerHTML = quiz.data[quiz.now].o[i];
-        label.setAttribute("for", "quizo" + i);
-        label.dataset.idx = i;
-        label.addEventListener("click", () => { quiz.select(label); });
-        quiz.hAns.appendChild(label);
-      }
-    },
-  
-    // (D) OPTION SELECTED
-    select: (option) => {
-      // (D1) DETACH ALL ONCLICK
-      let all = quiz.hAns.getElementsByTagName("label");
-      for (let label of all) {
-        label.removeEventListener("click", quiz.select);
-      }
-  
-      // (D2) CHECK IF CORRECT
-      let correct = option.dataset.idx == quiz.data[quiz.now].a;
-      if (correct) {
-        quiz.score++;
-        option.classList.add("correct");
+  },
+
+  // (D) OPTION SELECTED
+  select: (option) => {
+    // (D1) DETACH ALL ONCLICK
+    let all = quiz.hAns.getElementsByTagName("label");
+    for (let label of all) {
+      label.removeEventListener("click", quiz.select);
+    }
+
+    // (D2) CHECK IF CORRECT
+    let correct = option.dataset.idx == quiz.data[quiz.now].a;
+    if (correct) {
+      quiz.score++;
+      option.classList.add("correct");
+    } else {
+      option.classList.add("wrong");
+    }
+
+    // (D3) NEXT QUESTION OR END GAME
+    quiz.now++;
+    setTimeout(() => {
+      if (quiz.now < quiz.data.length) {
+        quiz.draw();
       } else {
-        option.classList.add("wrong");
+        quiz.hQn.innerHTML = `You have answered ${quiz.score} of ${quiz.data.length} correctly.`;
+        quiz.hAns.innerHTML = "";
       }
-  
-      // (D3) NEXT QUESTION OR END GAME
-      quiz.now++;
-      setTimeout(() => {
-        if (quiz.now < quiz.data.length) { quiz.draw(); }
-        else {
-          quiz.hQn.innerHTML = `You have answered ${quiz.score} of ${quiz.data.length} correctly.`;
-          quiz.hAns.innerHTML = "";
-        }
-      }, 1000);
-    },
-  
-    // (E) RESTART QUIZ
-    reset : () => {
-      quiz.now = 0;
-      quiz.score = 0;
-      quiz.draw();
-    }
-  };
+    }, 1000);
+  },
+
+  // (E) RESTART QUIZ
+  reset: () => {
+    quiz.now = 0;
+    quiz.score = 0;
+    quiz.draw();
+  },
+};
   window.addEventListener("load", quiz.init);
   
